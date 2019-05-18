94abd725d387111bc7333a5979cf7dacd473f29a
victor <victor@aula-ie.com>
2019-05-18 16:24:30 -0300

    ListView y CreateView

--- myapps/mascota/urls.py
+++ myapps/mascota/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from myapps.mascota.views import index, mascota_view, mascota_list
+from myapps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreate
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^nuevo$', mascota_view, name='mascota_crear'),
-    url(r'^listar', mascota_list, name='mascota_listar'),
+    url(r'^nuevo$', MascotaCreate.as_view(), name='mascota_crear'),
+    url(r'^listar', MascotaList.as_view(), name='mascota_listar'),
+    url(r'^editar/(?P<id_mascota>\d+)/$', mascota_edit , name='mascota_editar'),
+    url(r'^eliminar/(?P<id_mascota>\d+)/$', mascota_delete , name='mascota_eliminar'),
 ]
