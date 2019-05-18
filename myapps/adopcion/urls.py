cb8cc5d2373ba64ad13d41e7d9452396a98ac1ae
victor <victor@aula-ie.com>
2019-05-18 17:20:08 -0300

    CRUD con dos formularios

--- myapps/adopcion/urls.py
+++ myapps/adopcion/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
-from myapps.adopcion.views import index_adopcion
+from myapps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate, SolicitudUpdate, SolicitudDelete
 
 urlpatterns = [
     url(r'^index$', index_adopcion),
+    url(r'^solicitud/listar$', SolicitudList.as_view(), name='solicitud_listar'),
+    url(r'^solicitud/nueva$', SolicitudCreate.as_view(), name='solicitud_crear'),
+    url(r'^solicitud/editar/(?P<pk>\d+)$', SolicitudUpdate.as_view(), name='solicitud_editar'),
+    url(r'^solicitud/eliminar/(?P<pk>\d+)$', SolicitudDelete.as_view(), name='solicitud_eliminar'),    
 ]
